76bafe7e2319d6d9014640c92a1fa5e0e5af2a85
Magus <magus@bgforge.net>
2024-04-02 17:23:39 +0700

    Scripts are now in PATH

--- action.sh
+++ action.sh
@@ -3,13 +3,13 @@
 set -xeu -o pipefail
 
 if [[ "$INPUT_CHECK_SCRIPTS" == "true" ]]; then
-    ./scripts/scripts_lst.py "$INPUT_SCRIPTS_H" "$INPUT_SCRIPTS_LST"
+    scripts_lst.py "$INPUT_SCRIPTS_H" "$INPUT_SCRIPTS_LST"
 fi
 if [[ "$INPUT_CHECK_LVARS" == "true" ]]; then
-    ./scripts/lvars.py "$INPUT_SCRIPTS_LST" "$INPUT_SCRIPTS_DIR"
+    lvars.py "$INPUT_SCRIPTS_LST" "$INPUT_SCRIPTS_DIR"
 fi
 if [[ "$INPUT_CHECK_MSGS" == "true" ]]; then
-    ./scripts/dialogs.py "$INPUT_DIALOG_DIR" "$INPUT_SCRIPTS_DIR"
+    dialogs.py "$INPUT_DIALOG_DIR" "$INPUT_SCRIPTS_DIR"
 fi
 
 if [[ "$INPUT_WORLDMAP_PATH" != "false" ]]; then
@@ -19,5 +19,5 @@ if [[ "$INPUT_WORLDMAP_PATH" != "false" ]]; then
     fi
     # we speficically want word splitting
     # shellcheck disable=SC2086
-    ./scripts/worldmap.py "$INPUT_DIALOG_DIR" $sets_arg
+    worldmap.py "$INPUT_DIALOG_DIR" $sets_arg
 fi
