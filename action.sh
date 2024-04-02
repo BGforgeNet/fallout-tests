d77ef26f05c4faad0ba603fe120fb21d3e77a7e7
Magus <magus@bgforge.net>
2024-04-02 17:35:20 +0700

    fix lvars.py invocation

--- action.sh
+++ action.sh
@@ -6,7 +6,7 @@ if [[ "$INPUT_CHECK_SCRIPTS" == "true" ]]; then
     scripts_lst.py "$INPUT_SCRIPTS_H" "$INPUT_SCRIPTS_LST"
 fi
 if [[ "$INPUT_CHECK_LVARS" == "true" ]]; then
-    lvars.py "$INPUT_SCRIPTS_LST" "$INPUT_SCRIPTS_DIR"
+    lvars.py "$INPUT_SCRIPTS_DIR" "$INPUT_SCRIPTS_LST"
 fi
 if [[ "$INPUT_CHECK_MSGS" == "true" ]]; then
     dialogs.py "$INPUT_DIALOG_DIR" "$INPUT_SCRIPTS_DIR"
